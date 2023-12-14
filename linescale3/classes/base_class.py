4789371a11602ce870cac480b17cb7bc54fdbffc
Kyell Jensen <mail@kyelljensen.de>
2023-12-14 15:22:52 +0100

    treemotion.add_filename

--- linescale3/classes/base_class.py
+++ linescale3/classes/base_class.py
@@ -1,19 +1,17 @@
 from kj_core import get_logger
-from kj_core.classes.core_base_class import CoreBaseClass
 
 import linescale3
 
 logger = get_logger(__name__)
 
 
-class BaseClass(CoreBaseClass):
+class BaseClass:
     """
     Base class built upon CoreBaseClass, using specific managers from treemotion.
     """
 
     def __init__(self):
-        # Es wird angenommen, dass treemotion.CONFIG, usw. bereits initialisiert wurden
-        # Initialisiere CoreBaseClass mit treemotion-Managern
-        super().__init__(config=linescale3.CONFIG,
-                         plot_manager=linescale3.PLOT_MANAGER)
+        self.CONFIG = linescale3.CONFIG
+        self.PLOT_MANAGER = linescale3.PLOT_MANAGER
+
 
